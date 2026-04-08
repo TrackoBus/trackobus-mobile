81bd6adfcec9d515d93c079074e262064b4949e1
Sashika Hansani <sashikahansanii@gmail.com>
2026-04-08 16:01:51 +0530

    fix: update API base URL

--- constants/api.ts
+++ constants/api.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://192.168.1.9:8080/trck";
-//Change 192.168.1.9 to your Local IP Address
+export const API_BASE_URL = "http://10.76.87.174:8080/trck";
+//Change 10.76.87.174 to your Local IP Address
