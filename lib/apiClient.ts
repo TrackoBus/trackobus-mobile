12a557f145f5ecfed2c21605e1d4790415d0467e
SWM32 <98332411+SWM32@users.noreply.github.com>
2026-04-12 04:20:21 +0530

    Render routes

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -4,6 +4,7 @@ import axios, { AxiosHeaders } from "axios";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
